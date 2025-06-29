d81d6c1e29e8b956beee8996018075b2ab80cd2f
Adrien <chuadrien2003@gmail.com>
2025-06-29 02:28:50 +0200

    FEAT: CPP01 +ex02

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,24 +6,26 @@
 /*   By: achu <achu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 18:05:48 by achu              #+#    #+#             */
-/*   Updated: 2025/06/29 02:18:50 by achu             ###   ########.fr       */
+/*   Updated: 2025/06/29 02:27:45 by achu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include "Zombie.hpp"
-
-Zombie*		newZombie(std::string name);
-void		randomChump(std::string name);
 
 int	main()
 {
-	Zombie	*hunter;
+	std::string	brain = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &brain;
+	std::string &stringREF = brain;
 
-	hunter = newZombie("Hunter");
-	hunter->announce();
-	randomChump("Jockey");
-	delete hunter;
+	std::cout << "Memory adress of the string variable: " << &brain << std::endl;
+	std::cout << "Memory adress held by string pointer: " << stringPTR << std::endl;
+	std::cout << "Memory adress held by string reference: " << &stringREF << std::endl;
+
+	std::cout << "Value of the string variable: " << brain << std::endl;
+	std::cout << "Value pointed to by string pointer: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by string reference: " << stringREF << std::endl;
+
 	return (0);
 }
