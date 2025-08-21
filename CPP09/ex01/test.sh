dcad85e087672770c7986302380476e2dc505687
Adrien <chuadrien2003@gmail.com>
2025-08-21 11:37:32 +0200

    FIX: unit test +CPP09

--- CPP09/ex01/test.sh
+++ CPP09/ex01/test.sh
@@ -1,11 +1,12 @@
+make
+make clean
 clear
-set -x # echo on
 
 # failing tests
 echo "Failing tests:"
 ./RPN
 ./RPN ""
-./RPN efe wedwed
+./RPN "213" "324"
 ./RPN lkjjh
 ./RPN "+ 8 3 /"
 ./RPN "1 2"
@@ -18,9 +19,7 @@ echo "\n\nPassing tests:"
 ./RPN "1 2 +"
 ./RPN "1 2 -"
 ./RPN "9 8 *"
-./RPN "8 5 /"
 ./RPN "8 3 /"
-./RPN "8 4 /"
 
 #tests from subject
 echo "\n\nTests from subject:"
@@ -28,3 +27,5 @@ echo "\n\nTests from subject:"
 ./RPN "7 7 * 7 -"
 ./RPN "1 2 * 2 / 2 * 2 4 - +"
 ./RPN "(1 + 1)"
+
+make fclean
--- CPP09/ex02/test.sh
+++ CPP09/ex02/test.sh
@@ -21,5 +21,4 @@ echo "\nPassing tests:\n"
 ./PmergeMe 11 2 17 0 16 8 6 15 10 3 21 1 18 9 14 19 12 5 4 20 13 7
 ./PmergeMe $(seq 1 3000 | shuf)
 
-
 make fclean
